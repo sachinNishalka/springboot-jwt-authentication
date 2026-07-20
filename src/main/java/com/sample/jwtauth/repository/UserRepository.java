c36ecc013f184a4d80b1b00acdde2b6174bda66c
sachinNishalka <nishalkasachin@gmail.com>
2026-07-20 05:56:33 +0530

    implemented load user by username method

--- src/main/java/com/sample/jwtauth/repository/UserRepository.java
+++ src/main/java/com/sample/jwtauth/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.sample.jwtauth.repository;
 
 import com.sample.jwtauth.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
     Optional<UserEntity> findByEmail(String email);
--- src/main/java/com/sample/jwtauth/service/UserServiceImpl.java
+++ src/main/java/com/sample/jwtauth/service/UserServiceImpl.java
@@ -1,12 +1,24 @@
 package com.sample.jwtauth.service;
 
+
+import com.sample.jwtauth.entity.UserEntity;
+import com.sample.jwtauth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class UserServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        UserEntity user =  userRepository.findByEmail(email).orElseThrow(()->new UsernameNotFoundException("User not found witrh email "+email));
+        return  UserDetailsImpl.build(user);
     }
 }
